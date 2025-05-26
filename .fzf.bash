56d095f7496db2c7c1b97f00e572e61587d48774
Martin Edviken <martin.edviken@zenseact.com>
2025-05-26 16:26:26 +0200

    Fix typo of variable in .fzf.bash

--- .fzf.bash
+++ .fzf.bash
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+set e
+
 # Setup fzf
 # ---------
-fzf_base="~/.dotfiles/fzf"
+fzf_base="${HOME}/.dotfiles/fzf"
 fzf_base_esq=$(printf %q "$fzf_base")
 
-if [[ ! "$PATH" == *$fzf_base_esc/bin* ]]; then
+if [[ ! "$PATH" == *$fzf_base_esq/bin* ]]; then
   PATH="${PATH:+${PATH}:}$fzf_base/bin"
 fi
 
